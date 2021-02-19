b2fdefffbdb29a65805c814a9d29aa2b8f022bd6
Solatar <50340626+Solatar@users.noreply.github.com>
2021-02-19 16:54:03 +0100

    Update curlyBraces.R

--- R/curlyBraces.R
+++ R/curlyBraces.R
@@ -1,22 +1,21 @@
-
-
-#' creates a dataframe for a curled brace
+#' creates a dataframe for a curly brace
 #'
 #' @param xstart number, most left part of the brace
-#' @param xend number, most right part of the brace
+#' @param xend number, most right part of the brace. If pointing="side" the brace points towards this.
 #' @param ystart number, top end of the brace
-#' @param yend number, bottom end of the brace
+#' @param yend number, bottom end of the brace. If pointing="updown" (default) the brace points towards this.
 #' @param mid number, where the pointer is within the bracket space (between 0.25 and 0.75)
-#' @param pointing string, either "side" or "updown"
+#' @param pointing string, either "side" or "updown" (default)
 #' @param npoints integer, number of points generated for the brace curves (resolution). This number will be rounded to be a multiple of 4 for calculation purposes.
 #' @return data.frame with x and y columns that can be used as an input for ggplot or plotly
+#' @export
 #' @examples
 #' library(curlyBrace, ggplot2)
 #' mybrace <- seekBrace()
-#' ggplot() + geom_line(aes(x,y), data=mybrace, orientation="y")
+#' ggplot() + geom_line(aes(x,y), data=mybrace)
 #'
-#' mybrace <- seekBrace(xend=5, yend=1, pointing="updown")
-#' ggplot() + geom_line(aes(x,y), data=mybrace, orientation="x")
+#' mybrace <- seekBrace(pointing="side")
+#' ggplot() + geom_line(aes(x,y), data=mybrace, orientation="y")
 seekBrace <- function(xstart=0, xend=1, ystart=0, yend=5, mid=0.5, pointing="updown", npoints=100){
   if(mid<0.25){
     mid <- 0.25
@@ -70,6 +69,6 @@ seekBrace <- function(xstart=0, xend=1, ystart=0, yend=5, mid=0.5, pointing="upd
 
   mybrace <- do.call(rbind, rounds)
   mybrace <- mybrace[order(mybrace$x),] #fixes the ggplot-zickzack for the "updown" option
-  mybrace
+  return(mybrace)
 }
 
