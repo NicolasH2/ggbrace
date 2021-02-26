c66e84ea4b5019b47552ee34ee65527c009723bd
Solatar <50340626+Solatar@users.noreply.github.com>
2021-02-26 10:25:43 +0100

    Update seekBrace.R

--- R/seekBrace.R
+++ R/seekBrace.R
@@ -4,7 +4,7 @@
 #' @param xend number, most right part of the brace. If pointing="side" the brace points towards this.
 #' @param ystart number, top end of the brace
 #' @param yend number, bottom end of the brace. If pointing="updown" (default) the brace points towards this.
-#' @param mid number, where the pointer is within the bracket space (between 0.25 and 0.75)
+#' @param mid number, where the pointer is within the bracket space (will be forced between 0.25 and 0.75)
 #' @param pointing string, either "side" or "updown" (default)
 #' @param npoints integer, number of points generated for the brace curves (resolution). This number will be rounded to be a multiple of 4 for calculation purposes.
 #' @return data.frame with x and y columns that can be used as an input for ggplot or plotly
