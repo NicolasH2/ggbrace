8b80f305114b35d31cbf09eeb8b4ad9cbe1f1f0a
Solatar <50340626+Solatar@users.noreply.github.com>
2021-02-26 09:14:19 +0100

    Add files via upload

---
+++ R/geom_brace.R
@@ -0,0 +1,37 @@
+#' create a curly brace as a layer ready to be added to a ggplot object
+#'
+#' Imports:
+#' ggplot2
+#'
+#' @param xstart number, most left part of the brace
+#' @param xend number, most right part of the brace. If pointing="side" the brace points towards this.
+#' @param ystart number, top end of the brace
+#' @param yend number, bottom end of the brace. If pointing="updown" (default) the brace points towards this.
+#' @param mid number, where the pointer is within the bracket space (between 0.25 and 0.75)
+#' @param pointing string, either "side" or "updown" (default)
+#' @param npoints integer, number of points generated for the brace curves (resolution). This number will be rounded to be a multiple of 4 for calculation purposes.
+#' @return ggplot2 layer object (geom_path) that can directly be added to a ggplot2 object
+#' @export
+#' @examples
+#' library(curlyBrace, ggplot2)
+#' ggplot() + geom_brace()
+#'
+#' ggplot() + geom_brace(ystart=-5, pointing="side")
+#'
+#' ggplot() + geom_brace(color="red", size=3, linetype="dashed")
+geom_brace <- function(xstart=0, xend=1, ystart=0, yend=1, mid=0.5, pointing="updown", npoints=100, ...){
+
+  data <- curlyBraces::seekBrace(xstart, xend, ystart, yend, mid, pointing, npoints)
+
+  output <- ggplot2::layer(
+    data=data,
+    mapping=ggplot2::aes(x=x, y=y),
+    geom="path",
+    stat="identity",
+    position="identity",
+    show.legend=FALSE,
+    params=list(...)
+  )
+
+  return(output)
+}
